f1b941d8352929344c1d735e81cce31bc6b2b37e
Shintaro0806 <shin-taro@outlook.jp>
2024-11-07 11:21:31 +0900

    初回コミット

---
+++ src/test/java/StudentManegement/StudentManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package StudentManegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
