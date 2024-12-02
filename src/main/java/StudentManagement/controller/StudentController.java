0cf5babf44891533ac9d1b4e86c2c8afdda2f6e7
Shintaro0806 <shin-taro@outlook.jp>
2024-12-02 09:36:21 +0900

    第12回演習課題

--- src/main/java/StudentManagement/controller/StudentController.java
+++ src/main/java/StudentManagement/controller/StudentController.java
@@ -1,10 +1,14 @@
 package StudentManagement.controller;
 
 
+import StudentManagement.controller.converter.StudentConverter;
 import StudentManagement.data.Student;
 import StudentManagement.data.StudentCourse;
+import StudentManagement.domain.StudentDetail;
 import StudentManagement.service.StudentService;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourse> studentCourses = service.searchStudentCourseList();
+
+    return converter.convertStudentDetails(students, studentCourses);
   }
 
   @GetMapping("/studentcourseList")
