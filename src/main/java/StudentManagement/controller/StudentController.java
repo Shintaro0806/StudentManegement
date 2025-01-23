a09dd8e449363fe277f1476590aec0b96adb8f0c
Shintaro0806 <shin-taro@outlook.jp>
2025-01-23 10:44:31 +0900

    第23回演習課題

--- src/main/java/StudentManagement/controller/StudentController.java
+++ src/main/java/StudentManagement/controller/StudentController.java
@@ -84,6 +84,11 @@ public class StudentController {
     return ResponseEntity.ok("更新処理が成功しました。");
   }
 
+  /**
+   * 例外処理のテスト用のメソッドです。
+   * @param id　受講生ID
+   * @throws TestException エラーメッセージ
+   */
   @GetMapping("/testException/{id}")
   public void throwTestException(@PathVariable int id) throws TestException {
     throw new TestException("テスト用のメッセージです。");
